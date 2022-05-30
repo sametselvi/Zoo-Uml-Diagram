ce79d04c504326ce6e7ee4171d08b88c2fd24f0a
sametselvi <msametselvi@gmail.com>
2022-05-30 22:18:02 +0300

    The Prime Numbers

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Please enter any number for the inverted triangle : ");
         Scanner inp = new Scanner(System.in);
-
         int number;
 
+        System.out.print("Please enter any number : ");
         number = inp.nextInt();
 
+
         for (int i = 1; i <= number; i++) {
+            if (i == 2 || i == 3 || i == 5) {
 
 
-            for (int k = 1; k <=i; k++) {
-                System.out.print(" ");
+                System.out.println(i + " is the prime number.");
+            } else {
+
+                if (i == 1) {
+
+                    System.out.println(i + " is a simple number.");
+
+                } else if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+                    System.out.println(i + " is a simple number.");
+
+
+                } else {
+
+
+                    System.out.println(i +  " is the prime number.");
+
+
+                }
             }
-            for( int j =1 ; j <=2*(number -i) + 1; j++ ){
-
-                System.out.print("*");
-
-            }
-            System.out.println();
-
-
-
-
         }
     }
-
 }
+
+
+
