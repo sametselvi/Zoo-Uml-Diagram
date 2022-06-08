a2d0dcd1e31bcf21565ccb0773adfd1dd69ebc34
sametselvi <msametselvi@gmail.com>
2022-06-08 16:06:58 +0300

    palindromNumbers

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,87 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+    static void print(){
 
-        System.out.print("Please enter the number of digits : ");
-        int N = input.nextInt();
+            System.out.println("Please enter your number : ");
 
+    }
+    static boolean isPalindrom( int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
 
-        int number1 =0, number2 =1;
+    while ( temp !=0){
+        lastNumber = temp %10;
 
-        int counter =0;
+        reverseNumber = (reverseNumber*10) +lastNumber;
 
-        while (counter < N){
-
-            System.out.print(number1 + " ");
-            int num3 = number1 + number2;
-            number1 = number2;
-            number2 = num3;
-
-            counter = counter +1;
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+        temp/=10;
 
 
 
 
     }
+
+        if (reverseNumber ==number)
+            return true;
+        else
+            return false;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        print();
+        Scanner input = new Scanner(System.in);
+        int number =input.nextInt();
+        System.out.println(isPalindrom(number));
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
