673ffeeb14fc6e0e9f0b0e048d53a7ff98e3b03d
sametselvi <msametselvi@gmail.com>
2022-07-13 17:46:29 +0300

    harmonik

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,65 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen sayı giriniz : ");
+        int n = input.nextInt();
+
+        double[] list = new double[n];
+        double sum = 0.0;
+        for (double j = 1; j <= n; j++) {
+            for(int i =0; i< list.length; i++){
+                list[i]=j;
+
+            }
+            sum += 1/j;
 
 
-        Employee emp = new Employee("SAMET", 2000, 45, 1985);
+
+        }
+        System.out.println(sum);
 
 
-        emp.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*double sum =0.0;
+        for ( double i =1; i <= n; i++){
+            double k = 1/i;
+            System.out.println(k);
+
+        }
+
+
+
+
+
+       /* double[] liste2 = {1,2,3,4};
+
+        double sum=0.0;
+
+        for( int i =0; i< liste2.length;i++){
+         sum += 1/liste2[i];
+          System.out.println(sum);
+        }
+        double average = sum / liste2.length;
+         */
+
+
     }
 }
