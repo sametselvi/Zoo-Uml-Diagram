d154e9789eb800987f64b56eaa055842dc3c879d
sametselvi <msametselvi@gmail.com>
2022-07-20 13:24:51 +0300

    arrays dimension

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,29 @@
-
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    static boolean isFind(int[] arr, int value){
-        for(int i:arr ){
-            if(i==value){
-                return true;
-            }
-        }
-        return false;
-    }
+
+
     public static void main(String[] args) {
 
-        int[] list = {5, 6, 6, 7, 9, 10, 10, 100, 50, 100, 5, 99, 123, 50, 21, 7, 32,34,34};
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen dizinin boyutunu giriniz :");
+        int n = inp.nextInt();
+
+
+
+
+        int[] list = new int[n];
+
         int[] duplicate = new int[list.length];
-        int startIndex = 0;
+
         for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < list.length; j++) {
-
-                if ((i != j) && (list[i] == list[j])) {
-                    if(!isFind(duplicate, list[i])){
-
-                        duplicate[startIndex++] = list[i];
-                    }
-
-                    break;
-
-                }
-            }
-
+            System.out.print("Elemanı " + (i + 1) + "." + " giriniz :");
+            int n2 = inp.nextInt();
+            duplicate[i] = n2;
         }
-        for(int value: duplicate){
-
-            if(value !=0&& value%2==0){
-                System.out.println(value);
-
-
-
-            }
-
-
-
-        }
-
+        Arrays.sort(duplicate);
+        System.out.println(Arrays.toString(duplicate));
     }
 }
 
