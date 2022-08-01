813189bb36852ab1ea4c157bf3bbcce31c98f9ad
sametselvi <msametselvi@gmail.com>
2022-08-01 15:37:21 +0300

    Palindrom

--- src/Main.java
+++ src/Main.java
@@ -2,62 +2,91 @@ import java.util.*;
 
 
 public class Main {
+    static boolean isPalindrom(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrom2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return str.equals(reverse);
+
+    }
+    static boolean isPalindrom3(String str) {
+
+
+        char[] reverse2 = new char[str.length()];
+        char[] reverse3 = new char[str.length()];
+
+        int a = 0;
+
+
+        for (int j = str.length() - 1; j >= 0; j--) {
+            reverse2[a++] = str.charAt(j);
+            System.out.println(Arrays.toString(reverse2));
+
+
+        }
+
+        System.out.println("-------------------");
+
+        int b= 0;
+
+        for (int i =0; i <= str.length()-1; i++) {
+
+            reverse3[b++] = str.charAt(i);
+            System.out.println(Arrays.toString(reverse3));
+
+        }
+
+        if(Arrays.equals(reverse2,reverse3)){
+            System.out.println("Congrast two char arrays are the same :)");
+            return true;
+        }
+
+
+        System.out.println("Congrast two char arrays are the different !");
+
+        return false;
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
-        Random random = new Random();
-        Scanner inp = new Scanner(System.in);
-        int number = random.nextInt(100);
-        int right = 0;
-        int selected;
-        int[] wrong = new int[5];
-        boolean isWin = false;
-        boolean isMistake = false;
-        System.out.println(number);
-        while (right < 5) {
-            System.out.print("Lütfen tahminininizi giriniz : ");
-            selected = inp.nextInt();
-
-            if (selected < 0 || selected > 99) {
-                System.out.println("Lütfen sıfır ile 100 arasında değer giriniz !");
-                if (isMistake) {
-                    right++;
-                    System.out.println("Çok fazla hatalı giriz yaptınız. Kalan hakkınız : " + (5 - right));
 
 
-                } else {
 
-                    isMistake = true;
-                    System.out.println("Bir daha hatalı işleminizde hakkınız düşürülecektir!");
-                }
-                continue;
-            }
-            if (selected == number) {
-                System.out.println("Tebrikler doğru sayıyı buldunuz! " + "Tahmin ettiğiniz sayı : " + number);
-                isWin = true;
-                break;
-            } else {
-                System.out.println("Hatalı bir sayı girdiniz ! ");
-                if (selected > number) {
-                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
-                } else {
-                    System.out.println(selected + "sayısı, gizli sayıdan küçüktür.");
-                }
-
-                wrong[right++] = selected;
-
-                System.out.println("Kalan hakkı : " + (5 - right));
-            }
-
-        }
-        if ((!isWin)) {
-
-            System.out.println("Kaybettiniz ");
-
-                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
-
-
-        }
-
+        System.out.println(isPalindrom3("kayaks"));
 
     }
 
